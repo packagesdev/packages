20e4610ea5bbfe3e5fc57a446cb41827ac583436
packagesdev <stephane@MacPro.local>
2016-12-30 23:50:35 +0100

    Add subclass of NSTableView
    For the first click to be taken into account for a NSTextField

---
+++ shared/PKGTableView.h
@@ -0,0 +1,6 @@
+
+#import <Cocoa/Cocoa.h>
+
+@interface PKGTableView : NSTableView
+
+@end
---
+++ shared/PKGTableView.m
@@ -0,0 +1,11 @@
+
+#import "PKGTableView.h"
+
+@implementation PKGTableView
+
+- (BOOL)validateProposedFirstResponder:(NSResponder *)inResponder forEvent:( NSEvent *)inEvent
+{
+	return YES;
+}
+
+@end
