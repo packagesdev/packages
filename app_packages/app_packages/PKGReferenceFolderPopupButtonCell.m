423cadbc811c2b62f9d42e201ebd00d08b7df680
packagesdev <stephane@macpro.home>
2017-01-05 01:02:09 +0100

    Add new NSPopUpButtonCell subclasses
    Used for the Project Settings pane.

---
+++ app_packages/app_packages/PKGReferenceFolderPopupButtonCell.m
@@ -0,0 +1,59 @@
+/*
+Copyright (c) 2007-2016, Stephane Sudre
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+- Neither the name of the WhiteBox nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#import "PKGReferenceFolderPopupButtonCell.h"
+
+@implementation PKGReferenceFolderPopupButtonCell
+
+- (void)drawWithFrame:(NSRect)inFrame inView:(NSView *)inControlView
+{
+	[self drawBorderAndBackgroundWithFrame:inFrame inView:inControlView];
+	
+	// Draw the Path Type icon
+	
+	NSImage * tReferenceIcon=[NSImage imageNamed:@"ReferenceFolderRegularBackgroundUbuntu"];
+	NSRect tRect={
+		.origin=NSMakePoint(NSMinX(inFrame)+2.0,NSMinY(inFrame)+2.0),
+		.size=tReferenceIcon.size
+	};
+	
+	[tReferenceIcon drawInRect:tRect fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:(self.isEnabled==YES) ? 1.0 : 0.5 respectFlipped:YES hints:nil];
+	
+	// Draw the menu item
+	
+	NSMenuItem * tMenuItem=[self selectedItem];
+	
+	if (tMenuItem==nil)
+		return;
+	
+	NSImage * tIcon=[tMenuItem image];
+	
+	CGFloat tOffset=8.0f;
+	
+	if (tIcon!=nil)
+	{
+		NSRect tDestinationRect=NSMakeRect(NSMinX(inFrame)+tOffset,NSMinY(inFrame)+5.0,15.0,15.0);
+		
+		[tIcon drawInRect:tDestinationRect fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:(self.isEnabled==YES) ? 1.0 : 0.5 respectFlipped:YES hints:nil];
+	}
+	
+	tRect=[self titleRectForBounds:inFrame];
+	
+	tRect.origin.x=33.0;
+
+	tRect.size.width=NSMaxX(tRect)-NSMinX(tRect);
+	
+	[[tMenuItem title] drawInRect:tRect withAttributes:@{NSFontAttributeName:[self font]}];
+}
+
+@end
---
+++ app_packages/app_packages/PKGReferencePopupButtonCell.m
@@ -0,0 +1,128 @@
+/*
+Copyright (c) 2007-2016, Stephane Sudre
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+- Neither the name of the WhiteBox nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#import "PKGReferencePopupButtonCell.h"
+
+#import "PKGFilePathTypeMenu.h"
+
+#import "PKGFilePath.h"
+
+#define PKGReferencePopupButtonCellLeftOffset		20.0
+
+@interface PKGReferencePopupButtonCell ()
+
++ (NSImage *)leftCapForReferenceStyle:(PKGFilePathType)inPathType controlSize:(NSControlSize)inControlSize;
+
+@end
+
+@implementation PKGReferencePopupButtonCell
+
++ (NSImage *)leftCapForReferenceStyle:(PKGFilePathType)inPathType controlSize:(NSControlSize)inControlSize
+{
+	if (inControlSize==NSRegularControlSize)
+	{
+		switch(inPathType)
+		{
+			case PKGFilePathTypeAbsolute:
+				
+				return [NSImage imageNamed:@"AbsoluteRegularUbuntu"];
+				
+			case PKGFilePathTypeRelativeToProject:
+				
+				return [NSImage imageNamed:@"RelativeRegularUbuntu"];
+				
+			case PKGFilePathTypeRelativeToReferenceFolder:
+				
+				return [NSImage imageNamed:@"ReferenceFolderRegularUbuntu"];
+				
+			case PKGFilePathTypeMixed:
+				
+				return [NSImage imageNamed:@"MixedRegularUbuntu"];
+				
+			default:
+				
+				return nil;
+		}
+	}
+	
+	if (inControlSize==NSSmallControlSize)
+	{
+		switch(inPathType)
+		{
+			case PKGFilePathTypeAbsolute:
+				
+				return [NSImage imageNamed:@"AbsoluteSmallUbuntu"];
+				
+			case PKGFilePathTypeRelativeToProject:
+				
+				return [NSImage imageNamed:@"RelativeSmallUbuntu"];
+				
+			case PKGFilePathTypeRelativeToReferenceFolder:
+				
+				return [NSImage imageNamed:@"ReferenceFolderSmallUbuntu"];
+				
+			case PKGFilePathTypeMixed:
+				
+				return [NSImage imageNamed:@"MixedSmallUbuntu"];
+				
+			default:
+				
+				return nil;
+		}
+	}
+	
+	return nil;
+}
+
+- (id)initWithCoder:(NSCoder *)inCoder
+{
+	self=[super initWithCoder:inCoder];
+	
+	if (self!=nil)
+	{
+		self.menu=[PKGFilePathTypeMenu menuForAction:self.action target:self.target controlSize:self.controlSize];
+	}
+	
+	return self;
+}
+
+- (void)drawWithFrame:(NSRect) inFrame inView:(NSView *) inControlView
+{
+	[self drawBorderAndBackgroundWithFrame:inFrame inView:inControlView];
+	
+	NSMenuItem * tMenuItem=[self selectedItem];
+	
+	if (tMenuItem==nil)
+		return;
+	
+	// Draw the Path Type icon
+
+	NSImage * tReferenceIcon=[PKGReferencePopupButtonCell leftCapForReferenceStyle:[tMenuItem tag] controlSize:[self controlSize]];
+	NSRect tRect;
+	tRect.origin=NSMakePoint(NSMinX(inFrame)+2.0f,NSMinY(inFrame)+2.0f);
+	tRect.size=tReferenceIcon.size;
+	
+	[tReferenceIcon drawInRect:tRect fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:(self.isEnabled==YES) ? 1.0 : 0.5 respectFlipped:YES hints:nil];
+
+	// Draw the menu item
+	
+	CGFloat tOffset=PKGReferencePopupButtonCellLeftOffset+8.0f;
+	
+	tRect=[self titleRectForBounds:inFrame];
+	
+	tRect.origin.x=tOffset;
+	
+	[[tMenuItem title] drawInRect:tRect withAttributes:@{NSFontAttributeName:[self font]}];
+}
+
+@end
