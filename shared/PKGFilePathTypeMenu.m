a41a67db4d5ab14e846679691ede14739771d0de
packagesdev <stephane@macpro.home>
2017-01-08 23:22:01 +0100

    Add copyright comments

--- shared/PKGFilePathTypeMenu.m
+++ shared/PKGFilePathTypeMenu.m
@@ -1,3 +1,15 @@
+/*
+ Copyright (c) 2017, Stephane Sudre
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ 
+ - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ - Neither the name of the WhiteBox nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "PKGFilePathTypeMenu.h"
 
@@ -64,7 +76,7 @@
 
 + (instancetype)menuForAction:(SEL)inAction target:(id)inTarget controlSize:(NSControlSize)inControlSize
 {
-	PKGFilePathTypeMenu * tMenu=[[PKGFilePathTypeMenu alloc] init];
+	PKGFilePathTypeMenu * tMenu=[[PKGFilePathTypeMenu alloc] initWithTitle:@""];
 	
 	if (tMenu!=nil)
 	{
@@ -92,8 +104,6 @@
 		tMenuItem.image=[PKGFilePathTypeMenu _menuItemImageForPathType:PKGFilePathTypeAbsolute controlSize:inControlSize];
 		tMenuItem.target=inTarget;
 		
-		// Icon
-		
 		[tMenu addItem:tMenuItem];
 	}
 	
@@ -103,9 +113,7 @@
 + (NSSize)sizeOfPullDownImageForControlSize:(NSControlSize)inControlSize
 {
 	if (inControlSize==NSRegularControlSize)
-	{
 		return NSMakeSize(27.0,21.0);
-	}
 	
 	return NSZeroSize;
 }
