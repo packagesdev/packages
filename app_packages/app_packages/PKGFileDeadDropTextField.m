dfbe8cab04e3fc5ed1c0c51c1203d3d3fd56b946
packagesdev <stephane@macpro.home>
2017-01-08 23:25:02 +0100

    Update copyright comments

--- app_packages/app_packages/PKGFileDeadDropTextField.m
+++ app_packages/app_packages/PKGFileDeadDropTextField.m
@@ -1,3 +1,15 @@
+/*
+ Copyright (c) 2016, Stephane Sudre
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ 
+ - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ - Neither the name of the WhiteBox nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "PKGFileDeadDropTextField.h"
 
@@ -21,22 +33,18 @@
 
 - (NSDragOperation)draggingEntered:(id <NSDraggingInfo>)sender
 {
-	NSDragOperation sourceDragMask = [sender draggingSourceOperationMask];
 	NSPasteboard * tPasteBoard = [sender draggingPasteboard];
 	
 	if (self.deadDropDelegate!=nil && [[tPasteBoard types] containsObject:NSFilenamesPboardType]==YES)
 	{
+		NSDragOperation sourceDragMask = [sender draggingSourceOperationMask];
+		
 		if (sourceDragMask & NSDragOperationCopy)
 		{
 			NSArray * tFiles = [tPasteBoard propertyListForType:NSFilenamesPboardType];
 			
-			if (tFiles!=nil)
-			{
-				if ([self.deadDropDelegate fileDeadDropTextField:self validateDropFiles:tFiles]==YES)
-				{
-					return NSDragOperationCopy;
-				}
-			}
+			if (tFiles!=nil && [self.deadDropDelegate fileDeadDropTextField:self validateDropFiles:tFiles]==YES)
+				return NSDragOperationCopy;
 		}
 	}
 	
