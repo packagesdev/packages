82b87ceff5565b951c5cf338e82d0adb3eb0099b
packagesdev <stephane@macpro.home>
2017-01-11 01:30:04 +0100

    Subclasses of NSTableView and NSOutlineView
    To handle the delete key as best as possible.

--- shared/PKGOutlineView.m
+++ shared/PKGOutlineView.m
@@ -11,8 +11,33 @@
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import <Cocoa/Cocoa.h>
 
-@interface PKGTableView : NSTableView
+#import "PKGOutlineView.h"
+
+@implementation PKGOutlineView
+
+- (void)keyDown:(NSEvent *)inEvent
+{
+	unichar key = [[inEvent charactersIgnoringModifiers] characterAtIndex:0];
+	
+	// Handle Delete
+	
+	if (key == NSDeleteCharacter)
+	{
+		id tTarget=[NSApp targetForAction:@selector(delete:) to:nil from:self];
+		
+		if (tTarget!=nil)
+		{
+			NSMenuItem * tMenuItem=[[NSMenuItem alloc] initWithTitle:@"" action:@selector(delete:) keyEquivalent:@""];
+			
+			if ([tTarget validateMenuItem:tMenuItem]==YES)
+				[NSApp sendAction:@selector(delete:) to:tTarget from:self];
+			
+			return;
+		}
+	}
+	
+	[super keyDown:inEvent];
+}
 
 @end
