7f9f8236616ff14d419e3d468c7b9256ab7639ca
packagesdev <stephane@macpro.home>
2017-01-22 23:49:22 +0100

    Panel to display a certificate

---
+++ app_packages/app_packages/PKGCertificatePanel.m
@@ -0,0 +1,231 @@
+/*
+Copyright (c) 2007-2017, Stephane Sudre
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+- Neither the name of the WhiteBox nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#import "PKGCertificatePanel.h"
+
+#import <SecurityInterface/SFCertificateView.h>
+
+#define PKGCertificateWindowDisclosedDefaultHeight	606.0
+
+@interface PKGCertificateWindowController : NSWindowController
+{
+	IBOutlet SFCertificateView * _certificateView;
+	
+	IBOutlet NSButton * _defaultButton;
+	
+	BOOL _manuallyResized;
+	
+	CGFloat _defaultWindowHeight;
+}
+
+	@property (nonatomic) SecCertificateRef certificate;
+
+	@property (nonatomic,copy) NSString * prompt;
+
+
+- (IBAction)endDialog:(id)sender;
+
+- (void)refreshUI;
+
+// Notifications
+
+- (void)certificateViewDisclosureStateDidChange:(NSNotification *)inNotification;
+
+@end
+
+@implementation PKGCertificateWindowController
+
+- (void)dealloc
+{
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+#pragma mark -
+
+- (NSString *)windowNibName
+{
+	return @"PKGCertificatePanel";
+}
+
+- (void)windowDidLoad
+{
+	[super windowDidLoad];
+	
+	_defaultWindowHeight=self.window.frame.size.height;
+	
+	[_certificateView setDisplayTrust:NO];
+	
+	[_certificateView setEditableTrust:NO];
+	
+	[_certificateView setDisplayDetails:YES];
+	
+	[self refreshUI];
+	
+	// Register for notifications
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(certificateViewDisclosureStateDidChange:) name:@"SFCertificateViewDisclosureStateDidChange" object:_certificateView];
+}
+
+#pragma mark -
+
+- (void)setPrompt:(NSString *)inPrompt
+{
+	_prompt=[inPrompt copy];
+	
+	[self refreshUI];
+}
+
+- (void)setCertificate:(SecCertificateRef)inCertificate
+{
+	if (_certificate!=inCertificate)
+	{
+		_certificate=inCertificate;
+		
+		[self refreshUI];
+	}
+}
+
+#pragma mark -
+
+- (void)refreshUI
+{
+	if (_certificateView==nil)
+		return;
+	
+	[_certificateView setCertificate:_certificate];
+	
+	NSString * tTitle=self.prompt;
+	
+	if (tTitle==nil)
+		tTitle=NSLocalizedString(@"OK", @"");
+	
+	NSRect tButtonFrame=_defaultButton.frame;
+	
+	_defaultButton.title=tTitle;
+	
+	// Update button frame
+	
+	[_defaultButton sizeToFit];
+	
+	CGFloat tWidth=NSWidth(_defaultButton.frame);
+	
+	if (tWidth<PKGAppkitMinimumPushButtonWidth)
+		tWidth=PKGAppkitMinimumPushButtonWidth;
+	
+	tButtonFrame.origin.x=NSMaxX(tButtonFrame)-tWidth;
+	tButtonFrame.size.width=tWidth;
+	
+	_defaultButton.frame=tButtonFrame;
+}
+
+#pragma mark -
+
+- (IBAction)endDialog:(NSButton *)sender
+{
+	[NSApp endSheet:self.window returnCode:sender.tag];
+}
+
+#pragma mark - Notifications
+
+- (void)certificateViewDisclosureStateDidChange:(NSNotification *)inNotification
+{
+	if (_manuallyResized==YES)
+		return;
+	
+	NSRect tWindowFrame=self.window.frame;
+	CGFloat tNewHeight=([_certificateView detailsDisclosed]==YES) ? PKGCertificateWindowDisclosedDefaultHeight : _defaultWindowHeight;
+	
+	tWindowFrame.origin.y+=(tNewHeight-tWindowFrame.size.height);
+	tWindowFrame.size.height=tNewHeight;
+	
+	[self.window setFrame:tWindowFrame display:YES animate:YES];
+}
+
+- (NSSize)windowWillResize:(NSWindow *)inWindow toSize:(NSSize)inSize
+{
+	if (inWindow==self.window)
+		_manuallyResized=YES;
+	
+	return inSize;
+}
+
+@end
+
+@interface PKGCertificatePanel ()
+{
+	PKGCertificateWindowController * retainedWindowController;
+}
+
+- (void)_sheetDidEndSelector:(NSWindow *)inWindow returnCode:(NSInteger)inReturnCode contextInfo:(void *)contextInfo;
+
+@end
+
+@implementation PKGCertificatePanel
+
++ (PKGCertificatePanel *) certificatePanel
+{
+	PKGCertificateWindowController * tWindowController=[PKGCertificateWindowController new];
+	
+	PKGCertificatePanel * tPanel=(PKGCertificatePanel *)tWindowController.window;
+	
+	tPanel->retainedWindowController=tWindowController;
+	
+	return tPanel;
+}
+
+#pragma mark -
+
+- (SecCertificateRef)certificate
+{
+	return retainedWindowController.certificate;
+}
+
+- (void)setCertificate:(SecCertificateRef)inCertificate
+{
+	retainedWindowController.certificate=inCertificate;
+}
+
+- (NSString *)prompt
+{
+	return retainedWindowController.prompt;
+}
+
+- (void)setPrompt:(NSString *)inPrompt
+{
+	retainedWindowController.prompt=inPrompt;
+}
+
+#pragma mark -
+
+- (void)_sheetDidEndSelector:(PKGCertificatePanel *)inPanel returnCode:(NSInteger)inReturnCode contextInfo:(void *)contextInfo
+{
+	void(^handler)(void) = (__bridge_transfer void(^)(void)) contextInfo;
+	
+	if (handler!=nil)
+		handler();
+	
+	inPanel->retainedWindowController=nil;
+	
+	[inPanel orderOut:self];
+}
+
+- (void)beginSheetModalForWindow:(NSWindow *)inWindow completionHandler:(void (^)(void))handler
+{
+	[NSApp beginSheet:self
+	   modalForWindow:inWindow
+		modalDelegate:self
+	   didEndSelector:@selector(_sheetDidEndSelector:returnCode:contextInfo:)
+		  contextInfo:(__bridge_retained void*)[handler copy]];
+}
+
+@end
