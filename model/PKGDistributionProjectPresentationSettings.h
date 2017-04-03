b4f9d05eeec6eafab627eb55e9a31ffa0fef75f0
packagesdev <stephane@macpro.home>
2017-04-04 00:47:09 +0200

    Code layout

--- model/PKGDistributionProjectPresentationSettings.h
+++ model/PKGDistributionProjectPresentationSettings.h
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2016, Stephane Sudre
+ Copyright (c) 2016-2017, Stephane Sudre
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -34,7 +34,7 @@
 
 @interface PKGDistributionProjectPresentationSettings : NSObject <PKGObjectProtocol>
 
-	@property (readonly)NSMutableArray * sections;
+	@property (readonly) NSMutableArray * sections;
 
 	@property PKGPresentationTitleSettings * titleSettings;
 
