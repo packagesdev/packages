7b087ab5a3ae16d6b353fdeb8e6a7dfe065b5288
packagesdev <stephane@macpro.home>
2017-05-19 23:53:26 +0200

    NSPopUpButtonCell subclass for file path types

---
+++ app_packages/app_packages/PKGFilePathTypePopUpButtonCell.m
@@ -0,0 +1,42 @@
+
+#import "PKGFilePathTypePopUpButtonCell.h"
+
+#import "PKGFilePathTypeMenu.h"
+
+@implementation PKGFilePathTypePopUpButtonCell
+
+- (id)initWithCoder:(NSCoder *)inCoder
+{
+	self=[super initWithCoder:inCoder];
+	
+	if (self!=nil)
+	{
+		self.menu=[PKGFilePathTypeMenu menuForAction:nil target:nil controlSize:self.controlSize];
+		self.bordered=NO;
+	}
+	
+	return self;
+}
+
+#pragma mark -
+
+- (void)drawInteriorWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
+{
+	NSImage * tImage=self.selectedItem.image;
+	
+	if (tImage==nil)
+		return;
+	
+	CGFloat tMinX=NSMinX(cellFrame);
+	CGFloat tMiddleY=NSMidY(cellFrame);
+	
+	NSRect tDestinationRect;
+	tDestinationRect.size=tImage.size;
+	
+	tDestinationRect.origin.x=round(tMinX+7.0);
+	tDestinationRect.origin.y=round(tMiddleY-tImage.size.height*0.5)-1.0;
+	
+	[tImage drawInRect:tDestinationRect fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:(self.isEnabled==YES) ? 1.0 : 0.5 respectFlipped:YES hints:@{}];
+}
+
+@end
