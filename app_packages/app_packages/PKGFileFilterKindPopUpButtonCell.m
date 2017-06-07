d2a767d5686c069055f4a1ec2bc36db53fd86593
packagesdev <stephane@macpro.home>
2017-06-08 01:12:02 +0200

    Improve drawing of cell
    Will now display the correct arrows

--- app_packages/app_packages/PKGFileFilterKindPopUpButtonCell.m
+++ app_packages/app_packages/PKGFileFilterKindPopUpButtonCell.m
@@ -1,69 +1,53 @@
+/*
+ Copyright (c) 2017, Stephane Sudre
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ 
+ - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ - Neither the name of the WhiteBox nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "PKGFileFilterKindPopUpButtonCell.h"
 
 @implementation PKGFileFilterKindPopUpButtonCell
 
-- (void) drawWithFrame:(NSRect) cellFrame inView:(NSView *) controlView
+- (id)initWithCoder:(NSCoder *)inCoder
 {
+	self=[super initWithCoder:inCoder];
+	
+	if (self!=nil)
+	{
+		self.bordered=NO;
+	}
+	
+	return self;
+}
+
+#pragma mark -
+
+- (void)drawInteriorWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
+{
+	NSImage * tImage=self.selectedItem.image;
+	
+	if (tImage==nil)
+		return;
+	
+	tImage.size=NSMakeSize(14.0,14.0);
+	
 	CGFloat tMinX=NSMinX(cellFrame);
 	CGFloat tMiddleY=NSMidY(cellFrame);
 	
 	NSRect tDestinationRect;
+	tDestinationRect.size=tImage.size;
 	
-	tDestinationRect.origin.x=tMinX+18.0;
+	tDestinationRect.origin.x=round(tMinX+7.0);
+	tDestinationRect.origin.y=round(tMiddleY-tImage.size.height*0.5)-1.0;
 	
-	NSMenuItem * tMenuItem=[self selectedItem];
-	
-	if (tMenuItem!=nil)
-	{
-		NSImage * tImage=[[tMenuItem image] copy];
-		
-		NSRect tImageRect;
-		
-		[tImage setSize:NSMakeSize(14.0,14.0)];
-		
-		tImageRect.origin=NSZeroPoint;
-		tImageRect.size=tImage.size;
-		
-		tDestinationRect.size=tImageRect.size;
-		
-		tDestinationRect.origin.x=tMinX+8.0;
-		tDestinationRect.origin.y=round(tMiddleY-tDestinationRect.size.height*0.5-0.5);
-		
-		[tImage drawInRect:tDestinationRect fromRect:tImageRect operation:NSCompositeSourceOver fraction:(self.isEnabled==YES) ? 1.0 : 0.5 respectFlipped:YES hints:@{}];
-		
-		tDestinationRect.origin.x+=tImageRect.size.width+4;
-	}
-	
-	// Draw the Arrow
-	
-	// Top Arrow
-	
-	NSBezierPath * tPath=[NSBezierPath bezierPath];
-	
-	if ([self isEnabled]==YES)
-		[[NSColor colorWithDeviceWhite:0.15f alpha:1.0f] setFill];
-	else
-		[[NSColor colorWithDeviceWhite:0.15f alpha:0.5f] setFill];
-	
-	if (tPath!=nil)
-	{
-		[tPath moveToPoint:NSMakePoint(tDestinationRect.origin.x,round(tMiddleY)+1)];
-		[tPath lineToPoint:NSMakePoint(tDestinationRect.origin.x+5.0f,round(tMiddleY)+1)];
-		[tPath lineToPoint:NSMakePoint(tDestinationRect.origin.x+2.5f,round(tMiddleY+4.5f))];
-		[tPath closePath];
-		
-		[tPath fill];
-		
-		[tPath removeAllPoints];
-	
-		[tPath moveToPoint:NSMakePoint(tDestinationRect.origin.x,round(tMiddleY)-1.0f)];
-		[tPath lineToPoint:NSMakePoint(tDestinationRect.origin.x+5.0f,round(tMiddleY)-1.0f)];
-		[tPath lineToPoint:NSMakePoint(tDestinationRect.origin.x+2.5f,round(tMiddleY)-4.5f)];
-		[tPath closePath];
-		
-		[tPath fill];
-	}
+	[tImage drawInRect:tDestinationRect fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:(self.isEnabled==YES) ? 1.0 : 0.5 respectFlipped:YES hints:@{}];
 }
 
 @end
