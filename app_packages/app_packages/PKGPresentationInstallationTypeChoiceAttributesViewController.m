7ea1d679a764a8e3ff9cfe4a199a63717f8ac68f
packagesdev <stephane@macpro.home>
2017-06-30 01:00:50 +0200

    Show the Dependencies editor

--- app_packages/app_packages/PKGPresentationInstallationTypeChoiceAttributesViewController.m
+++ app_packages/app_packages/PKGPresentationInstallationTypeChoiceAttributesViewController.m
@@ -1,3 +1,15 @@
+/*
+ Copyright (c) 2017, Stephane Sudre
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ 
+ - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ - Neither the name of the WhiteBox nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "PKGPresentationInstallationTypeChoiceAttributesViewController.h"
 
@@ -5,11 +17,13 @@
 
 #import "PKGPresentationLocalizedStringsViewController.h"
 
+#import "PKGChoiceItemOptionsDependencies+UI.h"
 #import "PKGChoiceTreeNode+UI.h"
+#import "PKGPresentationInstallationTypeStepSettings+UI.h"
 
 #import "NSPopUpButton+OptimizedSize.h"
 
-#import "PKGPresentationInstallationTypeStepSettings+UI.h"
+
 
 typedef NS_ENUM(NSInteger, PKGVisibilityTag)
 {
@@ -306,9 +320,7 @@ typedef NS_ENUM(NSInteger, PKGVisibilityTag)
 		
 		dispatch_async(dispatch_get_main_queue(), ^{
 			
-			/*[[NSNotificationCenter defaultCenter] postNotificationName:ICDOCUMENT_PROJECT_PRESENTATION_INSTALLATION_TYPE_CHOICE_EDIT_DIDREQUEST
-																object:document_
-															  userInfo:nil];*/
+			[[NSNotificationCenter defaultCenter] postNotificationName:PKGChoiceItemOptionsDependenciesEditionWillBeginNotification object:self.document userInfo:@{PKGChoiceDependencyTreeNodeKey:self.choiceTreeNode}];
 		});
 	}
 	else
@@ -324,7 +336,10 @@ typedef NS_ENUM(NSInteger, PKGVisibilityTag)
 
 - (IBAction)editChoiceState:(id)sender
 {
-	// A COMPLETER
+	dispatch_async(dispatch_get_main_queue(), ^{
+		
+		[[NSNotificationCenter defaultCenter] postNotificationName:PKGChoiceItemOptionsDependenciesEditionWillBeginNotification object:self.document userInfo:@{PKGChoiceDependencyTreeNodeKey:self.choiceTreeNode}];
+	});
 }
 
 #pragma mark - PKGPresentationLocalizationsDataSourceDelegate
