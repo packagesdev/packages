bb7cddfb171e1f84e3165f61c12e03a5c6612018
packagesdev <stephane@macpro.home>
2017-07-06 00:52:20 +0200

    Add method to reset auxiliary data

--- model/PKGFileItem.h
+++ model/PKGFileItem.h
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2016, Stephane Sudre
+ Copyright (c) 2016-2017, Stephane Sudre
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -54,5 +54,7 @@ typedef NS_ENUM(NSInteger, PKGFileItemType)
 - (instancetype)initWithFileItem:(PKGFileItem *)inFileItem;
 
 - (instancetype)initWithFilePath:(PKGFilePath *)inFilePath uid:(uid_t)inUid gid:(gid_t)inGid permissions:(mode_t)inPermissions;
+
+- (void)resetAuxiliaryData;
 
 @end
--- model/PKGFileItem.m
+++ model/PKGFileItem.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2016, Stephane Sudre
+ Copyright (c) 2016-2017, Stephane Sudre
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -235,6 +235,14 @@ NSString * const PKGFileItemExpandedKey=@"EXPANDED";	// Let us know when the con
 	}
 	
 	return nFileItem;
+}
+
+#pragma mark -
+
+- (void)resetAuxiliaryData
+{
+	if (_fileItemAuxiliary!=nil)
+		_fileItemAuxiliary=nil;
 }
 
 @end
