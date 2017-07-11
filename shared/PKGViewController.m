cd7ef27a2a785b5e23b5359f7a0ab22a52298b4a
packagesdev <stephane@macpro.home>
2017-07-12 00:46:13 +0200

    Fix problem on OS X 10.8
    Some views would not load.

--- shared/PKGViewController.m
+++ shared/PKGViewController.m
@@ -19,25 +19,6 @@
 
 @implementation PKGViewController
 
-#if (MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_10)
-
-- (instancetype)initWithNibName:(NSString *)inNibName bundle:(NSBundle *)inBundle
-{
-	self=[super initWithNibName:inNibName bundle:inBundle];
-	
-	if (self!=nil)
-	{
-#if (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_10)
-		if (NSAppKitVersionNumber<NSAppKitVersionNumber10_10)
-#endif
-		[self.view setNextResponder:self];
-	}
-	
-	return self;
-}
-
-#endif
-
 - (void)dealloc
 {
 	// Remove observer
@@ -55,6 +36,15 @@
 - (void)loadView
 {
 	[super loadView];
+
+#if (MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_10)
+	
+#if (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_10)
+	if (NSAppKitVersionNumber<NSAppKitVersionNumber10_10)
+#endif
+		[self.view setNextResponder:self];
+
+#endif
 	
 	[self WB_viewDidLoad];
 }
