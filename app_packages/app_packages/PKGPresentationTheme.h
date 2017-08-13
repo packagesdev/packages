a21857b6a1157f41886d699826e75c1181c010a1
packagesdev <stephane@MacPro.local>
2017-08-13 20:13:39 +0200

    Add theme info

---
+++ app_packages/app_packages/PKGPresentationTheme.h
@@ -0,0 +1,14 @@
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger, PKGPresentationThemeVersion)
+{
+	PKGPresentationThemeMountainLion=0,
+	PKGPresentationThemeYosemite
+};
+
+extern NSString * const PKGPresentationTheme;
+
+
+extern NSString * const PKGPresentationThemeDidChangeNotification;
+
---
+++ app_packages/app_packages/PKGPresentationTheme.m
@@ -0,0 +1,6 @@
+
+#import "PKGPresentationTheme.h"
+
+NSString * const PKGPresentationTheme=@"ui.project.presentation.theme";
+
+NSString * const PKGPresentationThemeDidChangeNotification=@"PKGPresentationThemeDidChangeNotification";
