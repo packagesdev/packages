a7b2feb5d13805555a4ef6245fb05937ccc2c9ad
packagesdev <stephane@macpro.home>
2017-09-01 00:02:58 +0200

    Coding style

--- plugins/requirements/CPU/PKGRequirementViewControllerCPU/PKGRequirementViewControllerCPU/PKGRequirementViewControllerCPU.m
+++ plugins/requirements/CPU/PKGRequirementViewControllerCPU/PKGRequirementViewControllerCPU/PKGRequirementViewControllerCPU.m
@@ -1,3 +1,16 @@
+/*
+ Copyright (c) 2008-2017, Stephane Sudre
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ 
+ - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ - Neither the name of the WhiteBox nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #import "PKGRequirementViewControllerCPU.h"
 
 #import "PKGRequirement_CPU+Constants.h"
@@ -17,15 +30,15 @@
 	NSMutableDictionary * _settings;
 }
 
-- (IBAction)switchMinimumCPUCoresCount:(id) sender;
+- (IBAction)switchMinimumCPUCoresCount:(id)sender;
 
-- (IBAction)switchCPUArchitecture:(id) sender;
+- (IBAction)switchCPUArchitecture:(id)sender;
 
-- (IBAction)switchPowerPCArchitecture:(id) sender;
+- (IBAction)switchPowerPCArchitecture:(id)sender;
 
-- (IBAction)switchIntelArchitecture:(id) sender;
+- (IBAction)switchIntelArchitecture:(id)sender;
 
-- (IBAction)switchMinimumCPUFrequency:(id) sender;
+- (IBAction)switchMinimumCPUFrequency:(id)sender;
 
 @end
 
@@ -136,14 +149,14 @@
 
 #pragma mark -
 
-- (IBAction)switchMinimumCPUCoresCount:(NSSegmentedControl *) sender
+- (IBAction)switchMinimumCPUCoresCount:(NSSegmentedControl *)sender
 {
 	NSInteger tTag=[[sender cell] tagForSegment:sender.selectedSegment];
 	
 	_settings[PKGRequirementCPUMinimumCPUCoresCountKey]=@(tTag);
 }
 
-- (IBAction)switchCPUArchitecture:(NSSegmentedControl *) sender
+- (IBAction)switchCPUArchitecture:(NSSegmentedControl *)sender
 {
 	NSInteger tTag=[[sender cell] tagForSegment:sender.selectedSegment];
 	
@@ -189,21 +202,21 @@
 	}
 }
 
-- (IBAction)switchPowerPCArchitecture:(NSSegmentedControl *) sender
+- (IBAction)switchPowerPCArchitecture:(NSSegmentedControl *)sender
 {
 	NSInteger tTag=[[sender cell] tagForSegment:sender.selectedSegment];
 	
 	_settings[PKGRequirementCPUPowerPCArchitectureTypeKey]=@(tTag);
 }
 
-- (IBAction)switchIntelArchitecture:(NSSegmentedControl *) sender
+- (IBAction)switchIntelArchitecture:(NSSegmentedControl *)sender
 {
 	NSInteger tTag=[[sender cell] tagForSegment:sender.selectedSegment];
 	
 	_settings[PKGRequirementCPUIntelArchitectureTypeKey]=@(tTag);
 }
 
-- (IBAction)switchMinimumCPUFrequency:(NSPopUpButton *) sender
+- (IBAction)switchMinimumCPUFrequency:(NSPopUpButton *)sender
 {
 	NSInteger tTag=sender.selectedItem.tag;
 	
