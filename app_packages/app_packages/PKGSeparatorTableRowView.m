ae193389bb726b5d8581bd8455865dd0b2ee69e6
packagesdev <stephane@macpro.home>
2017-10-03 20:27:06 +0200

    Workaround for bug in NSTableView on OS X 10.8

--- app_packages/app_packages/PKGSeparatorTableRowView.m
+++ app_packages/app_packages/PKGSeparatorTableRowView.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2016, Stephane Sudre
+ Copyright (c) 2016-2017, Stephane Sudre
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -15,9 +15,18 @@
 
 @implementation PKGSeparatorTableRowView
 
+- (BOOL)isOpaque
+{
+	return NO;
+}
+
 - (void)drawBackgroundInRect:(NSRect)dirtyRect
 {
 	NSRect tBounds=self.bounds;
+	
+	[[NSColor whiteColor] set];
+	
+	NSRectFill(dirtyRect);
 	
 	[[NSColor colorWithDeviceWhite:0.702 alpha:1.0] set];
 	
