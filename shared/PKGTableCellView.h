91be2c61ee10e6b61b38dd5f60fd248df20374bc
packagesdev <stephane@MacPro.local>
2017-11-05 23:06:50 +0100

    New class to deal with insertNewLine: actions
    This class helps take care of the issue where typing return in a text
    field in a tableview is also triggering the OK button.

--- shared/PKGTableCellView.h
+++ shared/PKGTableCellView.h
@@ -11,35 +11,8 @@
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#import <Cocoa/Cocoa.h>
 
-#import "NSDictionary+DeepCopy.h"
-
-@implementation NSDictionary (WBDeepCopy)
-
-- (id)deepCopy
-{
-	NSError * tError=nil;
-	
-	NSData * tData=[NSPropertyListSerialization dataWithPropertyList:self format:NSPropertyListXMLFormat_v1_0 options:0 error:&tError];
-	
-	if (tData==nil)
-	{
-		if (tError!=nil)
-			NSLog(@"[NSDictionary deepCopy] : %@",tError.localizedDescription);
-		
-		return nil;
-	}
-	
-	NSPropertyListFormat tFormat;
-	NSDictionary * tDictionary=(NSDictionary *) [NSPropertyListSerialization propertyListWithData:tData options:0 format:&tFormat error:&tError];
-	
-	if (tDictionary==nil)
-	{
-		if (tError!=nil)
-			NSLog(@"[NSDictionary deepCopy] : %@",tError.localizedDescription);
-	}
-	
-	return tDictionary;
-}
+@interface PKGTableCellView : NSTableCellView <NSTextFieldDelegate>
 
 @end
