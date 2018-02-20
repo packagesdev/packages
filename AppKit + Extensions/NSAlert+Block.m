b176d9084d48188e2bb996f58b20a96a708176a3
packagesdev <stephane@macpro.home>
2018-02-20 23:48:09 +0100

    Code layout

--- AppKit + Extensions/NSAlert+Block.m
+++ AppKit + Extensions/NSAlert+Block.m
@@ -5,7 +5,7 @@
 
 - (void)WB_alertDidEndSelector:(NSAlert *)alert returnCode:(NSInteger)inReturnCode contextInfo:(void *)contextInfo
 {
-	void(^handler)(NSInteger) = (__bridge_transfer void(^)(NSInteger)) contextInfo;
+	void(^handler)(NSInteger)=(__bridge_transfer void(^)(NSInteger)) contextInfo;
 	
 	if (handler!=nil)
 		handler(inReturnCode);
@@ -14,13 +14,9 @@
 - (void)WB_beginSheetModalForWindow:(NSWindow *)inWindow completionHandler:(void (^)(NSModalResponse returnCode))handler
 {
 	[self beginSheetModalForWindow:inWindow
-					 modalDelegate: self
-					didEndSelector: @selector(WB_alertDidEndSelector:returnCode:contextInfo:)
-					   contextInfo: (__bridge_retained void*)[handler copy] ];
+					 modalDelegate:self
+					didEndSelector:@selector(WB_alertDidEndSelector:returnCode:contextInfo:)
+					   contextInfo:(__bridge_retained void*)[handler copy]];
 }
-
-
-
-
 
 @end
