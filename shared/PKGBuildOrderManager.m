25c1e13c9cc6f21dc844f54e390be3912ca29a58
packagesdev <stephane@macpro.home>
2018-04-04 00:32:53 +0200

    Better handle the XPC connection disconnection
    This solves one issue of the build command not working for an opened
    document after having updated Packages.

--- shared/PKGBuildOrderManager.m
+++ shared/PKGBuildOrderManager.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2016, Stephane Sudre
+ Copyright (c) 2016-2018, Stephane Sudre
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -66,6 +66,16 @@
 		_dispatcherConnection.remoteObjectInterface=[NSXPCInterface interfaceWithProtocol:@protocol(PKGBuildDispatcherInterface)];
 		
 		__weak NSXPCConnection *tWeakDispatcherConnection=_dispatcherConnection;
+		
+		tWeakDispatcherConnection.interruptionHandler=^{
+			
+			NSXPCConnection *tStrongConnection=tWeakDispatcherConnection;
+			
+			if (tStrongConnection!=nil)
+				tStrongConnection.invalidationHandler = nil;
+			
+			_dispatcherConnection=nil;
+		};
 		
 		tWeakDispatcherConnection.invalidationHandler=^{
 			
