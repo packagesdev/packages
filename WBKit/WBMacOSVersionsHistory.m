2a8b51cc417a83201ed0e9d8de450044c661e4e5
packagesdev <stephane@macpro.home>
2018-04-27 23:29:30 +0200

    Bug fix
    There actually is a 10.12.6 version of macOS.

--- WBKit/WBMacOSVersionsHistory.m
+++ WBKit/WBMacOSVersionsHistory.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2017, Stephane Sudre
+ Copyright (c) 2017-18, Stephane Sudre
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -335,7 +335,7 @@
 					
 				case WBMacOSSierraMinorVersion:
 					
-					return NSMakeRange(0, 6);
+					return NSMakeRange(0, 7);
 					
 				case WBMacOSHighSierraMinorVersion:
 					
