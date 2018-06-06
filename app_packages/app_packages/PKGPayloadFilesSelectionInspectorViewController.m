7b91e159f76eab8dc5762922d91aba259313502f
packagesdev <stephane@macpro.home>
2018-06-07 00:18:13 +0200

    Set document during init
    The document was nil during the first refresh of the UI and therefore
    the absolute path could not be computed in some cases.

--- app_packages/app_packages/PKGPayloadFilesSelectionInspectorViewController.m
+++ app_packages/app_packages/PKGPayloadFilesSelectionInspectorViewController.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2017, Stephane Sudre
+ Copyright (c) 2017-2018, Stephane Sudre
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -87,7 +87,7 @@
 	
 	tView=_scriptsTabViewItem.view;
 	
-	tTabViewItemViewController=[PKGPayloadFilesSelectionInspectorScriptsViewController new];
+	tTabViewItemViewController=[[PKGPayloadFilesSelectionInspectorScriptsViewController alloc] initWithDocument:self.document];
 	
 	if (tTabViewItemViewController==nil)
 	{
