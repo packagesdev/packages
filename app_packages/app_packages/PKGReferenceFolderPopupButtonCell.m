4e394762c1b77a8444c52c57a67b9ec216130a6c
packagesdev <stephane@MacPro.local>
2018-07-12 22:29:35 +0200

    Draw with controlTextColor
    This lets the rendering be OK in Dark Mode.

--- app_packages/app_packages/PKGReferenceFolderPopupButtonCell.m
+++ app_packages/app_packages/PKGReferenceFolderPopupButtonCell.m
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2007-2017, Stephane Sudre
+Copyright (c) 2007-2018, Stephane Sudre
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -53,7 +53,8 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 	tRect.size.width=NSMaxX(tRect)-NSMinX(tRect);
 	
-	[[tMenuItem title] drawInRect:tRect withAttributes:@{NSFontAttributeName:[self font]}];
+	[[tMenuItem title] drawInRect:tRect withAttributes:@{NSFontAttributeName:[self font],
+														 NSForegroundColorAttributeName:[NSColor controlTextColor]}];
 }
 
 @end
