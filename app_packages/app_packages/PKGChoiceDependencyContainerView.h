3d1e3131a1be3c477a0d813aa574fa706756d304
packagesdev <stephane@MacPro.local>
2018-07-12 22:20:43 +0200

    Copyright update
    Update copyright and fix code alignment

--- app_packages/app_packages/PKGChoiceDependencyContainerView.h
+++ app_packages/app_packages/PKGChoiceDependencyContainerView.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2007-2017, Stephane Sudre
+Copyright (c) 2007-2018, Stephane Sudre
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -17,7 +17,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 @class PKGPresentationInstallationTypeChoiceDependenciesViewController;
 
-	@interface PKGChoiceDependencyContainerView : NSView
+@interface PKGChoiceDependencyContainerView : NSView
 
 	@property PKGChoiceDependencyTreeNode * dependencyTreeNode;
 	@property PKGChoiceDependencyTreeLogicNode * parentDependencyTreeNode;
