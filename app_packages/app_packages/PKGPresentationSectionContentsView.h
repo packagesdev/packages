757cb537b5fe5a52d91758ce9e06cc68f54d2ebd
packagesdev <stephane@MacPro.local>
2018-11-01 00:36:27 +0100

    Rename class

--- app_packages/app_packages/PKGPresentationSectionContentsView.h
+++ app_packages/app_packages/PKGPresentationSectionContentsView.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2007-2017, Stéphane Sudre
+Copyright (c) 2007-2018, Stéphane Sudre
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -13,7 +13,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 #import "PKGControlledView.h"
 
-@interface PKGPresentationContentsView : NSBox <PKGControlledView>
+@interface PKGPresentationSectionContentsView : NSBox <PKGControlledView>
 
 
 @end
--- app_packages/app_packages/PKGPresentationSectionContentsView.m
+++ app_packages/app_packages/PKGPresentationSectionContentsView.m
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2007-2017, Stéphane Sudre
+Copyright (c) 2007-2018, Stéphane Sudre
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -11,7 +11,7 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#import "PKGPresentationContentsView.h"
+#import "PKGPresentationSectionContentsView.h"
 
 #import "PKGInstallerApp.h"
 
@@ -19,7 +19,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 #import "PKGDocument.h"
 
-@interface PKGPresentationContentsView ()
+@interface PKGPresentationSectionContentsView ()
 
 - (void)updateTheme;
 
@@ -29,7 +29,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 @end
 
-@implementation PKGPresentationContentsView
+@implementation PKGPresentationSectionContentsView
 
 - (void)dealloc
 {
