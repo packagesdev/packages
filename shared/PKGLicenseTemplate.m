b013b9774c0caf3a9a5c7d6bc040059629c32039
packagesdev <dev.packages@gmail.com>
2021-11-12 22:54:13 +0100

    Bump copyright year

--- shared/PKGLicenseTemplate.m
+++ shared/PKGLicenseTemplate.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2017, Stephane Sudre
+ Copyright (c) 2017-2021, Stephane Sudre
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
