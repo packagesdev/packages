e60c4ac3087a25fcd7f9bbdf58222d7ab88ef4ab
packagesdev <dev.packages@gmail.com>
2025-08-18 22:09:28 +0200

    Update Xcode project settings and use dot notation where it makes sense

--- plugins/requirements/RAM/PKGRequirementConverterRAM/PKGRequirementConverterRAM/PKGRequirementConverterRAM.m
+++ plugins/requirements/RAM/PKGRequirementConverterRAM/PKGRequirementConverterRAM/PKGRequirementConverterRAM.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2008-2016, Stephane Sudre
+ Copyright (c) 2008-2025, Stephane Sudre
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -63,7 +63,7 @@ static unsigned long long sRAMConversionTable[RAM_CONVERSION_COUNT]={
 		return nil;
 	}
 	
-	NSInteger tIndex=[tNumber integerValue];
+	NSInteger tIndex=tNumber.integerValue;
 	
 	if (tIndex<0 || tIndex>=RAM_CONVERSION_COUNT)
 	{
--- plugins/requirements/RAM/PKGRequirementViewControllerRAM/PKGRequirementViewControllerRAM/PKGRequirementViewControllerRAM.m
+++ plugins/requirements/RAM/PKGRequirementViewControllerRAM/PKGRequirementViewControllerRAM/PKGRequirementViewControllerRAM.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2008-2020, Stephane Sudre
+ Copyright (c) 2008-2025, Stephane Sudre
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -49,7 +49,7 @@
 	NSNumber * tNumber=_settings[PKGRequirementRAMMinimumSizeIndexKey];
 	
 	if (tNumber!=nil)
-		tTag=[tNumber integerValue];
+		tTag=tNumber.integerValue;
 	
 	if ([_minimumRAMPopUpButton selectItemWithTag:tTag]==NO)
 	{
